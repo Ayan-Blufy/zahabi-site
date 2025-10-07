804623963ec25d2e18a5ff1765e497378d3ac85a
Ayan <ayan@emertech.io>
2025-10-07 21:13:48 +0530

    update: the image in mobile view

--- src/components/home/WhyUs.tsx
+++ src/components/home/WhyUs.tsx
@@ -1,6 +1,6 @@
 const WhyUsImage = () => {
   return (
-    <div className="w-full lg:min-h-screen min-h-[auto] flex justify-center items-center px-6 py-8 md:min-h-[auto]">
+    <div className="w-full lg:min-h-screen min-h-[auto] flex justify-center items-center  py-8 md:min-h-[auto]">
   
       <img
         src="https://zahabi-test-78997878.s3.ap-south-1.amazonaws.com/RWA+(3).png"
@@ -10,9 +10,9 @@ const WhyUsImage = () => {
 
   
       <img
-        src="https://zahabi-test-78997878.s3.ap-south-1.amazonaws.com/RWA+(4).png"
+        src="https://my-tf-test-bucket-ayan0093.s3.us-east-1.amazonaws.com/Frame+1000002475.png"
         alt="Why Us Mobile"
-        className="block md:hidden w-full h-full object-contain"
+        className="block md:hidden w-full h-full  object-contain"
       />
     </div>
   );
